1f7100467a63cc949842cb63ef8c6899829da9d8
devnull03 <56480041+devnull03@users.noreply.github.com>
2023-12-10 17:05:59 -0800

    feat: ass8 q1

--- assignment8/Main.java
+++ assignment8/Main.java
@@ -1,14 +1,26 @@
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap();
+        UnsortedTableMap<String, Integer> map = new UnsortedTableMap<>();
         map.put("Alice", 25);
         map.put("Bob", 30);
-        map.put("Charlie", 45);
+        map.put("Charlie", 35);
 
-        System.out.println(map.get("Alice"));
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "'s age is " + entry.getValue());
+        }
+
+        System.out.println("Removed Bob's age: " + map.remove("Bob") );
+        map.put("Eva", 40);
+        System.out.println("is map empty? " + map.isEmpty());
+
+        System.out.println("Entries in map: ");
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        System.out.println("Size of map: " + map.size());
 
     }
 
